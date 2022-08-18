dd4b33b0248ca38d70bcfd7b322c66e954154a76
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-18 05:02:41 +0300

    commit tree 824084a14bac64b046bd9640f018586c5ce4dd1e

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,25 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    def initialize (brand)
+        @brand = brand
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        def condition= (condition="new")
+            @condition = condition
+        end
+
+        def condition
+            @condition
+        end
+    end
+end
+shoe =Shoe.new ("SubShooter")
+shoe.color="red"
+shoe.size =12
+shoe.material = "Leather"
+shoe.condition = "tattered"
+shoe.cobble
+shoe.condition
