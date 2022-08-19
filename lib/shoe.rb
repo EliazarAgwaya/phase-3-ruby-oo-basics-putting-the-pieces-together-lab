c127dbd326802558dfab134c25e2d98497a66f30
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-08-19 09:45:42 +0300

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+    def initialize (brand)
+        @brand = brand
+    end
+    def cobble
+        @condition="new"
+        puts "Your shoe is as good as new!"
+    end
+end
+shoe =Shoe.new ("SubShooter")
+shoe.color="red"
+shoe.size =12
+shoe.material = "Leather"
+shoe.condition = "tattered"
+shoe.cobble
+shoe.condition
